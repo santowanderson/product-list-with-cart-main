6cbf688c8472edccd08f8bfe15dc844022240ad3
santowanderson <wandersonsantospereira21@gmail.com>
2024-08-11 16:05:15 -0300

    tailwindcss style configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Red: "hsl(14, 86%, 42%)",
+        Green: "hsl(159, 69%, 38%)",
+        "rose-50": "hsl(20, 50%, 98%)",
+        "rose-100": "hsl(13, 31%, 94%)",
+        "rose-300": "hsl(14, 25%, 72%)",
+        "rose-400": "hsl(7, 20%, 60%)",
+        "rose-500": "hsl(12, 20%, 44%)",
+        "rose-900": "hsl(14, 65%, 9%)",
+      }
+    },
   },
   plugins: [],
 }
